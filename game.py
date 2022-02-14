e5613338e001b77a8bbcca6d7169b083224749fa
suhys <yuseung.suh@gmail.com>
2022-02-13 18:16:52 -0800

    Create bullets

--- game.py
+++ game.py
@@ -1,21 +1,33 @@
 import sys
 import pygame
 import game_function as gf
+from pygame.sprite import Group  
 
 class Settings():
     """ A clas to store all settings for Alien Invasion"""
     
     def __init__(self):
-        """Initialize the game's settings"""
+        """Initialize the game's settings""" 
         #Screen settings
         self.screen_width = 1000
         self.screen_height = 600 
         self.bg_color = (230,230,230)
         
+        # ship settings
+        self.ship_speed_factor = 1.5
+        
+        #Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60,60,60 
+        
+        
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position"""
         self.screen = screen
+        self.ai_settings = ai_settings
         
         #Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -26,16 +38,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         
+        #Store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+        
         #Movement lfag
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         """Update the ship's position based on the movment flag"""
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -= 1
+        # Update the ship's center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+            
+        # Update rect object from self.center
+        self.rect.centerx = self.center
         
     def blitme(self):
         """draw the ship at its current location"""
@@ -51,14 +70,17 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     
     #Make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    #Make a group to store bullets in 
+    bullets = Group()
     
     # Start the main loop for the game.
     while True:
         #Watch for keyboard and mouse events.
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
         
 run_game()
         
--- game_function.py
+++ game_function.py
@@ -1,30 +1,49 @@
 import sys
 import pygame
+from bullets import Bullet
 
-def check_events(ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        #Create a new bullet and add it to the bullets group
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+        
+def check_keyup_events(event, ship):
+    """Respond to key releases"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False 
+    
+
+
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
             
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move the ship to the right
-                ship.rect.centerx += 1
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-                
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    ship.moving_left = False 
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
 
-def update_screen(ai_settings, screen, ship):
+                
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+                
+
+def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw he screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
+    
+    # Redraw all bullets behind ship an aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
                 
     #Make the most recently drawn screen visible

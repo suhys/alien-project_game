5dc6d3a11fe3cb368405b51d5952f0cb0187662b
suhys <yuseung.suh@gmail.com>
2022-03-06 17:42:00 -0800

    Update alien - add rows

--- game.py
+++ game.py
@@ -24,7 +24,7 @@ def run_game():
     aliens = Group()
     
     # Make a alien
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
         
     # Start the main loop for the game
     while True:
